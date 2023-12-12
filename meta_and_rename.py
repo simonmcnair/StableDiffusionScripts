67d6a8fd0a92ff1ef4832924a4afe621ef51d93f
101189766+simonmcnair@users.noreply.github.com <101189766+simonmcnair@users.noreply.github.com>
2023-12-12 10:52:38 +0000

    Updating file - Revision 2

--- meta_and_rename.py
+++ meta_and_rename.py
@@ -1,51 +1,42 @@
 import os
 import shutil
 from PIL import Image
+import re
+from datetime import datetime
 
+def extract_text_after2(list_obj, text):
+    for element in list_obj:
+        if element.strip().lower().startswith(text.lower() + ":"):
+            return element.split(":")[-1].strip()
+    return None
 
-def move_to_subfolder(path, subfolder):
-    # Check if the path is a directory or a file
+def getmodel(mystr):
+    test = parameter.split(",")
+    try:
+        res = extract_text_after2(test,"Model")
+    except:
+        return None
+    return res
 
-    last_folder = os.path.basename(os.path.dirname(path))
+def getseed(mystr):
+    test = parameter.split(",")
+    try:
+        res = extract_text_after2(test,"Seed")
+    except:
+        return None
+    return res
 
-    if subfolder in last_folder:
-        print(path + " directory tree already contains " + subfolder)
-        return
-
-    if os.path.isdir(path):
-        # Create the subfolder if it doesn't exist
-        subfolder_path = os.path.join(path, subfolder)
-        if not os.path.exists(subfolder_path):
-            os.makedirs(subfolder_path)
-
-        # Move all files in the directory to the subfolder
-        for file in os.listdir(path):
-            if os.path.isfile(os.path.join(path, file)):
-                base_name, ext = os.path.splitext(file)
-                dest_file = os.path.join(subfolder_path, file)
-                count = 1
-                while os.path.exists(dest_file):
-                    new_name = f"{base_name}_{count}{ext}"
-                    dest_file = os.path.join(subfolder_path, new_name)
-                    count += 1
-                shutil.move(os.path.join(path, file), dest_file)
-
-    elif os.path.isfile(path):
-        # Create the subfolder if it doesn't exist
-        subfolder_path = os.path.join(os.path.dirname(path), subfolder)
-        if not os.path.exists(subfolder_path):
-            os.makedirs(subfolder_path)
-
-        # Move the file to the subfolder
-        base_name, ext = os.path.splitext(os.path.basename(path))
-        dest_file = os.path.join(subfolder_path, os.path.basename(path))
-        count = 1
-        while os.path.exists(dest_file):
-            new_name = f"{base_name}_{count}{ext}"
-            dest_file = os.path.join(subfolder_path, new_name)
-            count += 1
-        shutil.move(path, dest_file)
-
+def get_sanitized_download_time(filepath):
+    # Get the modification time of the file
+    mtime = os.path.getmtime(filepath)
+    # Convert the modification time to a datetime object
+    dt = datetime.fromtimestamp(mtime)
+    # Format the datetime as a string in the format YYYY-MM-DD_HH-MM-SS
+    dt_string = dt.strftime("%Y-%m-%d_%H-%M-%S")
+    # Replace any spaces or colons with underscores
+    sanitized_dt_string = dt_string.replace(" ", "_").replace(":", "_")
+    # Return the sanitized datetime string
+    return sanitized_dt_string
 
 path = cwd = os.getcwd()
 path = "c:\\users\\simon\\Downloads\\stable-diffusion\\consolidated"
@@ -58,6 +49,9 @@ for root, dirs, files in os.walk(path):
         
             badfile = False
             hasparameters = False
+            parameter = None
+            model = None
+            seed = None
 
             if filename.endswith(".png"):
                 with Image.open(item_path) as img:
@@ -65,6 +59,7 @@ for root, dirs, files in os.walk(path):
                         parameter = img.info.get("parameters")
                         if parameter is not None:
                             print(filename + " has metadata.")
+                            hasparameters = True
                         else:
                             print("PNG with no metadata")
                             badfile = True
@@ -76,9 +71,76 @@ for root, dirs, files in os.walk(path):
                 print("Ignoring unsupported filetype: " + filename)
                 continue
 
-            if badfile==True:
-                print(filename + " has no metadata.  Moving to nometa subdirectory")
-                move_to_subfolder(item_path,"nometa")
+            if hasparameters==True:
+                model = getmodel(parameter)
+
+                seed = getseed(parameter)
+                datetimemod = get_sanitized_download_time(item_path)
+
+                if '<lora:' in parameter:
+                    # Use a regular expression to find all words between lora: and :?>
+                    matches = re.findall(r'lora:(.*?):', parameter)
+                    Loras = '_'.join(matches)
+
+
+                    if model is not None and seed is not None:
+                        new_filename = model + '_' + seed + '_' + datetimemod + '_Loras_' + Loras + os.path.splitext(filename)[1]
+                        new_item_path = os.path.join(root, new_filename)
+
+                        print(new_item_path)
+
+                        try:
+                            if not os.path.samefile(item_path, new_item_path):
+                                    shutil.move(item_path, new_item_path)
+                        except Exception as e:
+                            print(str(e))
+                    elif seed is not None:
+                        new_filename = seed + '_' + datetimemod + '_Loras_' + Loras + os.path.splitext(filename)[1]
+                        new_item_path = os.path.join(root, new_filename)
+
+                        print(new_item_path)
+
+                        try:
+                            if not os.path.samefile(item_path, new_item_path):
+                                    shutil.move(item_path, new_item_path)
+                        except Exception as e:
+                            print(str(e))
+
+                    else:
+                        print("seed or model is blank.  Skipping")
+                else:
+                    print("uses no Loras")
+
+                    if model is not None and seed is not None:
+                        new_filename = model + '_' + seed + '_' + datetimemod + os.path.splitext(filename)[1]
+                        new_item_path = os.path.join(root, new_filename)
+
+                        print(new_item_path)
+
+                        try:
+                            if not os.path.samefile(item_path, new_item_path):
+                                    shutil.move(item_path, new_item_path)
+                        except Exception as e:
+                            print(str(e))
+                    elif seed is not None:
+                        new_filename = seed + '_' + datetimemod + '_Loras_' + Loras + os.path.splitext(filename)[1]
+                        new_item_path = os.path.join(root, new_filename)
+
+                        print(new_item_path)
+
+                        try:
+                            if not os.path.samefile(item_path, new_item_path):
+                                    shutil.move(item_path, new_item_path)
+                        except Exception as e:
+                            print(str(e))
+                    else:
+                        print("Parameters but no seed or model")
             else:
-                print(filename + " has metadata.  Moving to meta subdirectory")
-                move_to_subfolder(item_path,"meta")
+                if 'nometa' not in item_path:
+                    print("should this be in this folder ?")
+                else:
+                    print("no parameters")
+
+
+
+
