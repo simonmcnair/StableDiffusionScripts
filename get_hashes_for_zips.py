8a71b50b557f9d624342b8826258ea15aa0e2dff
101189766+simonmcnair@users.noreply.github.com <101189766+simonmcnair@users.noreply.github.com>
2023-12-12 09:31:08 +0000

    Updating file - Revision 4

--- get_hashes_for_zips.py
+++ get_hashes_for_zips.py
@@ -1,59 +1,142 @@
 import os
-import csv
-from collections import defaultdict
+from collections import Counter
 from PIL import Image
+import csv
 
-positive_prompts = defaultdict(int)
-negative_prompts = defaultdict(int)
-extra_prompts = defaultdict(int)
+# Define the output file
+output_file = "metadata.csv"
 
-metadata_tags = ["parameters", "Steps", "Sampler", "CFG scale", "Seed", "Size", "Model hash", "Model"]
+# Initialize the counters for positive, negative, and extra prompts
+positive_counter = Counter()
+negative_counter = Counter()
+extra_counter = Counter()
 
-# Loop through all PNG files in the current working directory
-for filename in os.listdir("."):
+
+def process_string(input_string,var):
+    # Replace ", " with ","
+    input_string = input_string.replace(", ", ",")
+    # Replace " ," with ","
+    input_string = input_string.replace(" ,", ",")
+    # Remove excess spaces from beginning and end of string
+    input_string = input_string.strip()
+    # Replace "\n\n" with "\n"
+    input_string = input_string.replace("\n\n", "\n")
+    # Split string into an array using "\n" as delimiter
+    output_array = input_string.split(var)
+    return output_array
+
+def remove_negative_prompt(input_string,removal):
+    output_string = input_string.replace(removal, '').strip()
+    return output_string
+
+def remove_empty_dict_values(input_dict):
+    output_dict = {k: v for k, v in input_dict.items() if v}
+    return output_dict
+
+def remove_empty_values(input_list):
+    output_list = [value for value in input_list if value != ""]
+    return output_list
+
+#def find_prompt(input_list, prompt):
+#    try:
+#        index = input_list.index(prompt)
+#        return index
+#    except ValueError:
+#        return None
+import csv
+
+def write_array_to_csv(array, filename, header=None):
+    with open(filename, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        if header:
+            writer.writerow(header)
+        for row in array:
+            writer.writerow(row)
+
+
+def sort_dictionary(dictionary):
+    sorted_keys = sorted(dictionary.keys(), key=lambda x: (x.split(":")[0], -dictionary[x]))
+    sorted_dict = {key: dictionary[key] for key in sorted_keys}
+    return sorted_dict
+
+def prefix_list(input_list, prefix):
+    output_list = [prefix + str(value) for value in input_list]
+    return output_list
+
+def find_prompt(input_list, search_text):
+    try:
+        for i, value in enumerate(input_list):
+            if search_text in value:
+                return i
+        return -1
+    except ValueError:
+        return None
+
+def aggregate_data(destination, addition):
+    for value in addition:
+        if value in destination:
+            destination[value] += 1
+        else:
+            destination[value] = 1
+    return destination
+
+destinationarr = {}
+# Loop over all PNG files in the current working directory
+for filename in os.listdir():
     if filename.endswith(".png"):
-        # Open the image file and extract the metadata
+        # Open the image and extract the metadata
+        print("Processing " + filename)
         with Image.open(filename) as img:
             metadata = img.info.get("parameters", "")
 
-        # Split the metadata string into individual prompts
-        prompts = metadata.split(",")
+        metadata = process_string(metadata,"\n")
 
-        # Categorize each prompt as positive, negative, or extra
-        category = "extra"
-        for prompt in prompts:
-            if "Negative prompt:" in prompt:
-                category = "negative"
-            elif category == "extra":
-                category = "positive"
+        positiveprompt = process_string(metadata[0],",")
+        positiveprompt = remove_empty_values(positiveprompt)
+        positiveprompt = prefix_list(positiveprompt,"Positive Prompt:")
 
-            # Strip whitespace and any colon characters from the prompt
-            prompt = prompt.strip().replace(":", "")
+        res = find_prompt(metadata,'Negative prompt: ')
+        if res is None:
+            print("The string 'Negative prompt' was not found in the list.")
+        else:
+            print("The index of the first occurrence of 'Negative prompt' is:", res)
+                        
+            if 'Negative prompt:' in metadata[res]:
+                negativeprompt = remove_negative_prompt(metadata[res],'Negative prompt:')
+                negativeprompt = process_string(negativeprompt,",")
+                negativeprompt = remove_empty_values(negativeprompt)
+            else:
+                print("No negative prompt")
+        negativeprompt = prefix_list(negativeprompt,"Negative Prompt:")
 
-            # Increment the count for the prompt in the appropriate category
-            if prompt:
-                if category == "positive":
-                    positive_prompts[prompt] += 1
-                elif category == "negative":
-                    negative_prompts[prompt] += 1
-                else:
-                    extra_prompts[prompt] += 1
 
-        # Extract additional metadata tags
-        for tag in metadata_tags:
-            value = img.info.get(tag, "")
-            value = value.strip().replace(":", "")
-            if value:
-                extra_prompts[value] += 1
+        res = find_prompt(metadata,'Steps: ')
+        if res is None:
+            print("The string 'Steps' was not found in the list.")
+        else:
+            print("The index of the first occurrence of 'Steps' is:", res)
+            extra = metadata[res].split(",")
 
-# Write the prompt counts to a CSV file
-with open("prompt.csv", "w", newline="") as csvfile:
-    writer = csv.writer(csvfile)
-    writer.writerow(["prompt", "type", "count"])
-    for prompt, count in sorted(positive_prompts.items(), key=lambda x: x[1], reverse=True):
-        writer.writerow([prompt, "positive", count])
-    for prompt, count in sorted(negative_prompts.items(), key=lambda x: x[1], reverse=True):
-        writer.writerow([prompt, "negative", count])
-    for prompt, count in sorted(extra_prompts.items(), key=lambda x: x[1], reverse=True):
-        if prompt not in positive_prompts and prompt not in negative_prompts:
-            writer.writerow([prompt, "extra", count])
+        result = positiveprompt + negativeprompt + extra
+   
+            #if 'Negative prompt:' in metadata[res]:
+            #    negativeprompt = remove_negative_prompt(metadata[res],'Negative prompt:')
+            #    negativeprompt = process_string(negativeprompt,",")
+            #    negativeprompt = remove_empty_values(negativeprompt)
+            #else:
+            #    print("No negative prompt")
+    sorte_dictionary = aggregate_data(destinationarr,result)    
+
+
+sorte_dictionary = sort_dictionary(sorte_dictionary)
+    # Sort the data by count in descending order
+
+#destinationarr =     sorted_dict = dict(sorted(destinationarr.items(), key=lambda x: (x[0].split(":")[0], -int(x[0].split(",")[1]))))
+ 
+head = {"setting", "description","count"}
+
+write_array_to_csv(sorte_dictionary,output_file, head)
+# Write the data to the output file
+
+
+
