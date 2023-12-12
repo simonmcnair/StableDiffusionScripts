71909a7f996d04334c39084c7299ad399c2f31a7
101189766+simonmcnair@users.noreply.github.com <101189766+simonmcnair@users.noreply.github.com>
2023-12-12 09:31:10 +0000

    Updating file - Revision 7

--- get_hashes_for_zips.py
+++ get_hashes_for_zips.py
@@ -6,6 +6,9 @@ from collections import Counter
 # Define the keys that contain comma-separated values
 array_keys = ['prompt', 'negative_prompt']
 
+# Initialize counters for each array key
+array_counters = {key: Counter() for key in array_keys}
+
 # Loop over each PNG file in the working directory
 for filename in glob.glob('*.png'):
     with open(filename, 'rb') as f:
@@ -16,6 +19,8 @@ for filename in glob.glob('*.png'):
                 key, value = chunk[1].decode('utf-8').split('\0')
                 if key in array_keys:
                     metadata[key] = value.split(',')
+                    # Increment the counters for each value in the array
+                    array_counters[key].update(metadata[key])
                 else:
                     metadata[key] = value
 
@@ -23,13 +28,12 @@ for filename in glob.glob('*.png'):
     with open(f'{filename}.csv', 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
         for key, value in metadata.items():
-            if key in array_keys:
-                # Count the occurrences of values for array keys
-                counter = Counter(value)
-                # Write the counts to a separate CSV file
-                with open(f'{key}.csv', 'a', newline='') as countfile:
-                    countwriter = csv.writer(countfile)
-                    for item, count in counter.items():
-                        countwriter.writerow([filename, item, count])
-            else:
+            if key not in array_keys:
                 writer.writerow([key, value])
+
+# Write the final counts to CSV files
+for key, counter in array_counters.items():
+    with open(f'{key}.csv', 'w', newline='') as countfile:
+        countwriter = csv.writer(countfile)
+        for item, count in counter.items():
+            countwriter.writerow([item, count])
