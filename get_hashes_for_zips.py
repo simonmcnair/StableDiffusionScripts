ce882b53851b8e7f197304ea9dd5d54ea18ff2f8
101189766+simonmcnair@users.noreply.github.com <101189766+simonmcnair@users.noreply.github.com>
2023-12-12 09:31:07 +0000

    Updating file - Revision 3

--- get_hashes_for_zips.py
+++ get_hashes_for_zips.py
@@ -1,63 +1,59 @@
 import os
+import csv
 from collections import defaultdict
-from PIL import Image, PngImagePlugin
+from PIL import Image
 
-# initialize prompt counts dictionary
-prompt_counts = defaultdict(lambda: defaultdict(int))
+positive_prompts = defaultdict(int)
+negative_prompts = defaultdict(int)
+extra_prompts = defaultdict(int)
 
-# loop through all PNG files in current working directory
-for filename in os.listdir('.'):
-    if filename.endswith('.png'):
-        # open image file
-        with Image.open(filename) as im:
-            # extract PNG text chunks
-            chunks = PngImagePlugin.PngInfo(im.info).text
+metadata_tags = ["parameters", "Steps", "Sampler", "CFG scale", "Seed", "Size", "Model hash", "Model"]
 
-            # process each text chunk
-            for chunk in chunks:
-                # extract parameters and negative prompt if present
-                if 'parameters' in chunk:
-                    params_str = chunk.split('parameters,')[1]
-                    params = [p.strip() for p in params_str.split(',') if p.strip()]
-                    for p in params:
-                        prompt_counts['positive'][p] += 1
-                if 'Negative prompt' in chunk:
-                    neg_str = chunk.split('Negative prompt:')[1]
-                    neg = [n.strip() for n in neg_str.split(',') if n.strip()]
-                    for n in neg:
-                        prompt_counts['negative'][n] += 1
+# Loop through all PNG files in the current working directory
+for filename in os.listdir("."):
+    if filename.endswith(".png"):
+        # Open the image file and extract the metadata
+        with Image.open(filename) as img:
+            metadata = img.info.get("parameters", "")
 
-                # extract other prompts
-                if 'Steps:' in chunk:
-                    steps_str = chunk.split('Steps:')[1]
-                    steps = steps_str.split(',')[0].strip()
-                    prompt_counts['extra']['steps,' + steps] += 1
-                if 'Sampler:' in chunk:
-                    sampler_str = chunk.split('Sampler:')[1]
-                    sampler = sampler_str.split(',')[0].strip()
-                    prompt_counts['extra']['Sampler,' + sampler] += 1
-                if 'CFG scale:' in chunk:
-                    cfg_str = chunk.split('CFG scale:')[1]
-                    cfg = cfg_str.split(',')[0].strip()
-                    prompt_counts['extra']['CFG scale,' + cfg] += 1
-                if 'Seed:' in chunk:
-                    seed_str = chunk.split('Seed:')[1]
-                    seed = seed_str.split(',')[0].strip()
-                    prompt_counts['extra']['Seed,' + seed] += 1
-                if 'Size:' in chunk:
-                    size_str = chunk.split('Size:')[1]
-                    size = size_str.split(',')[0].strip()
-                    prompt_counts['extra']['Size,' + size] += 1
-                if 'Model hash:' in chunk:
-                    hash_str = chunk.split('Model hash:')[1]
-                    model_hash = hash_str.split(',')[0].strip()
-                    prompt_counts['extra']['Model hash,' + model_hash] += 1
-                if 'Model:' in chunk:
-                    model_str = chunk.split('Model:')[1]
-                    model = model_str.split(',')[0].strip()
-                    prompt_counts['extra']['Model,' + model] += 1
+        # Split the metadata string into individual prompts
+        prompts = metadata.split(",")
 
-# print cumulative prompt counts
-for prompt_type, prompt_vals in prompt_counts.items():
-    for prompt_val, count in sorted(prompt_vals.items(), key=lambda x: x[1], reverse=True):
-        print(f'{prompt_val},{prompt_type},{count}')
+        # Categorize each prompt as positive, negative, or extra
+        category = "extra"
+        for prompt in prompts:
+            if "Negative prompt:" in prompt:
+                category = "negative"
+            elif category == "extra":
+                category = "positive"
+
+            # Strip whitespace and any colon characters from the prompt
+            prompt = prompt.strip().replace(":", "")
+
+            # Increment the count for the prompt in the appropriate category
+            if prompt:
+                if category == "positive":
+                    positive_prompts[prompt] += 1
+                elif category == "negative":
+                    negative_prompts[prompt] += 1
+                else:
+                    extra_prompts[prompt] += 1
+
+        # Extract additional metadata tags
+        for tag in metadata_tags:
+            value = img.info.get(tag, "")
+            value = value.strip().replace(":", "")
+            if value:
+                extra_prompts[value] += 1
+
+# Write the prompt counts to a CSV file
+with open("prompt.csv", "w", newline="") as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(["prompt", "type", "count"])
+    for prompt, count in sorted(positive_prompts.items(), key=lambda x: x[1], reverse=True):
+        writer.writerow([prompt, "positive", count])
+    for prompt, count in sorted(negative_prompts.items(), key=lambda x: x[1], reverse=True):
+        writer.writerow([prompt, "negative", count])
+    for prompt, count in sorted(extra_prompts.items(), key=lambda x: x[1], reverse=True):
+        if prompt not in positive_prompts and prompt not in negative_prompts:
+            writer.writerow([prompt, "extra", count])
