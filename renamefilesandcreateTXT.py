32708ffb8cfb95fb006442fe39c7a6179ffb34aa
101189766+simonmcnair@users.noreply.github.com <101189766+simonmcnair@users.noreply.github.com>
2023-12-12 09:39:54 +0000

    Updating file - Revision 4

--- renamefilesandcreateTXT.py
+++ renamefilesandcreateTXT.py
@@ -1,7 +1,66 @@
 import os
-from PIL import Image
-
 import shutil
+import re
+from PIL import Image, ExifTags
+from PIL.ExifTags import TAGS
+from PIL.ExifTags import Base
+import exifread
+
+from datetime import datetime
+
+def sanitise_filename(filename):
+    # Remove any characters that are not allowed in filenames
+    filename = re.sub(r'[^\w\s\-_\.]', '', filename).strip().lower()
+    # Replace any spaces with underscores
+    filename = re.sub(r'\s+', '_', filename)
+    # Remove any consecutive underscores
+    filename = re.sub(r'_{2,}', '_', filename)
+    # Return the sanitized filename
+    basefilename, extension = os.path.splitext(filename)
+    if len(basefilename) > 200:
+        filename = basefilename[:200] + extension
+    #filename = filename.lower()
+    return filename
+
+
+def get_jpeg_comments(filename):
+    with Image.open(filename) as img:
+        comments = img.info.get('comments', None)
+    return comments
+
+def copy_file_modification_time(filea, fileb):
+    # Get the modification time of filea
+    mtime = os.path.getmtime(filea)
+    
+    # Set the modification time of fileb to match filea
+    shutil.copystat(filea, fileb)
+    os.utime(fileb, (os.path.getatime(fileb), mtime))
+
+
+def get_jpeg_exif_comments(filename):
+    try:
+        with Image.open(filename) as img:
+            exif_data = img._getexif()
+            user_comment = exif_data.get(37510)
+            com = user_comment.decode('utf-8').rstrip('\x00')
+            com = com.replace('\x00','').replace('UNICODE','')
+    except:
+        return None
+    return com
+    
+
+def get_sanitized_download_time(filepath):
+    # Get the modification time of the file
+    mtime = os.path.getmtime(filepath)
+    # Convert the modification time to a datetime object
+    dt = datetime.fromtimestamp(mtime)
+    # Format the datetime as a string in the format YYYY-MM-DD_HH-MM-SS
+    dt_string = dt.strftime("%Y-%m-%d_%H-%M-%S")
+    # Replace any spaces or colons with underscores
+    sanitized_dt_string = dt_string.replace(" ", "_").replace(":", "_")
+    # Return the sanitized datetime string
+    return sanitized_dt_string
+
 
 def move_to_subfolder(path, subfolder):
     # Check if the path is a directory or a file
@@ -47,41 +106,87 @@ def extract_text_after(list_obj, text):
 
 def extract_text_after2(list_obj, text):
     for element in list_obj:
-        if element.strip().startswith(text + ":"):
+        if element.strip().lower().startswith(text.lower() + ":"):
             return element.split(":")[-1].strip()
     return None
 
+def getmodel(mystr):
+    test = parameter.split(",")
+    try:
+        res = extract_text_after2(test,"Model")
+    except:
+        return None
+
+    return res
+       
 
 for filename in os.listdir("."):
     badfile = False
+    changeto = get_sanitized_download_time(filename)
+
     if filename.endswith(".png"):
         with Image.open(filename) as img:
-            parameter = img.info.get("parameters")
-            if parameter is not None:
-                print(filename + " has metadata.")
-
-                test = parameter.split(",")
-                result = extract_text_after2(test,"Model")
-                if result is None:
-                    print("No Model specified in " + filename)
-                    result = "No_model_specified_"
+            try:
+                parameter = img.info.get("parameters")
+                if parameter is not None:
+                    print(filename + " has metadata.")
+                    model = getmodel(parameter)
                 else:
-                    output_filename = result + "_" + os.path.splitext(filename)[0] + ".txt"
-                    
-                with open(output_filename, "w", encoding="utf-8") as output_file:
-                    output_file.write(parameter)
-                        #output_file.write(parameter)
-            else:
+                    print("PNG with no metadata")
+                    badfile = True
+            except:
                 badfile = True
+    elif filename.endswith(".jpeg") or filename.endswith(".jpg"):
+        print("jpeg.  Extracting comments from: " + filename)
+       # retval = get_jpeg_comments(filename)
+       # if retval is None:
+       #     print("no comments")
+       # else:
+       #     print (retval) 
+        try:  
+            parameter = get_jpeg_exif_comments(filename)
+            model = getmodel(parameter)
+           # input()
+        except Exception as e:
+            print(str(e))
+            badfile = True
+    else:
+        print("Ignoring unsupported filetype: " + filename)
+        continue
 
     if badfile==True:
-        print(filename + " has no metadata.  Moving")
+        print(filename + " has no metadata.  Moving to nometa subdirectory")
         move_to_subfolder(filename,"nometa")
     if badfile==False:
-            print(filename + " metadata extracted.  Moving")
-            try:
-                shutil.move(filename, result+ "_" + filename)
-            except Exception as e:
-                print("File move failed with error " + e)
 
-       
+        if model is None:
+            print("No Model specified in " + filename)
+            model = "No_model_specified_"
+        else:
+            output_filename = sanitise_filename(model + "_" + changeto + ".txt")
+            print("found model: " + model + ".  Outputting parameters to " + output_filename)
+            
+            try:
+                with open(output_filename, "w", encoding="utf-8") as output_file:
+                    print("Trying to write metadata to " + output_filename)
+                    output_file.write(parameter)
+                    #output_file.write(parameter)
+                copy_file_modification_time(filename,output_filename)
+                
+            except:
+                output_filename = sanitise_filename("complexmodelfield_" + changeto + ".txt")
+                print("shouldn't get here " + output_filename)
+                with open(output_filename, "w", encoding="utf-8") as output_file:
+                    output_file.write(parameter)
+
+        try:
+            moveto = sanitise_filename(model + "_" + changeto + ".png")
+            if os.path.exists(model+ "_" + filename):
+                print("File already exists")                
+            else:
+                print(filename + " metadata extracted.  Moving to " + moveto)
+                shutil.move(filename, moveto)
+        except Exception as e:
+            print("File move failed with error " + str(e))
+
+ 
