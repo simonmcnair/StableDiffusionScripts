960d79191a27e66cb47533e15175a8a1d79ef50c
101189766+simonmcnair@users.noreply.github.com <101189766+simonmcnair@users.noreply.github.com>
2023-12-12 09:31:13 +0000

    Updating file - Revision 9

--- get_hashes_for_zips.py
+++ get_hashes_for_zips.py
@@ -1,44 +1,62 @@
+from PIL import Image
 import os
 import csv
-from PIL import Image
 
-# create the CSV files
-overview_file = open("overview.csv", "w", newline="")
-overview_writer = csv.writer(overview_file)
-overview_writer.writerow(["filename", "metadata"])
+# Initialize the counters for the prompt and negative_prompt tags
+prompt_counts = {}
+negative_prompt_counts = {}
 
-prompt_file = open("prompt.csv", "w", newline="")
-prompt_writer = csv.writer(prompt_file)
-prompt_writer.writerow(["tag:value", "count"])
+# Open the output CSV files for writing
+with open("overview.csv", "w", newline="", encoding="utf-8") as overview_file, \
+     open("prompt.csv", "w", newline="", encoding="utf-8") as prompt_file, \
+     open("negative_prompt.csv", "w", newline="", encoding="utf-8") as negative_prompt_file:
 
-negative_prompt_file = open("negative_prompt.csv", "w", newline="")
-negative_prompt_writer = csv.writer(negative_prompt_file)
-negative_prompt_writer.writerow(["tag:value", "count"])
+    # Create CSV writers for each file
+    overview_writer = csv.writer(overview_file)
+    prompt_writer = csv.writer(prompt_file)
+    negative_prompt_writer = csv.writer(negative_prompt_file)
 
-# loop through all PNG files in the current directory
-for filename in os.listdir("."):
-    if filename.endswith(".png"):
-        # open the PNG file and extract its metadata
-        img = Image.open(filename)
-        metadata = img.info
-        
-        # write the metadata to the overview CSV file
-        overview_writer.writerow([filename, metadata])
-        
-        # process the parameters and negative_prompt tags
-        if "parameters" in metadata:
-            parameters = metadata["parameters"].replace("\n", "").replace("\r", "")
-            parameters_dict = dict(item.split(":") for item in parameters.split(", "))
-            for tag_value, count in sorted(parameters_dict.items(), key=lambda x: int(x[1]), reverse=True):
-                prompt_writer.writerow([tag_value, count])
-        
-        if "negative_prompt" in metadata:
-            negative_prompt = metadata["negative_prompt"].replace("\n", "").replace("\r", "")
-            negative_prompt_dict = dict(item.split(":") for item in negative_prompt.split(", "))
-            for tag_value, count in sorted(negative_prompt_dict.items(), key=lambda x: int(x[1]), reverse=True):
-                negative_prompt_writer.writerow([tag_value, count])
-                
-# close the CSV files
-overview_file.close()
-prompt_file.close()
-negative_prompt_file.close()
+    # Write the headers for the CSV files
+    overview_writer.writerow(["Filename", "Metadata"])
+    prompt_writer.writerow(["Tag", "Value", "Count"])
+    negative_prompt_writer.writerow(["Tag", "Value", "Count"])
+
+    # Traverse the PNG files in the current working directory
+    for filename in os.listdir("."):
+        if filename.endswith(".png"):
+            # Open the PNG file and extract the metadata
+            with Image.open(filename) as img:
+                metadata = img.info
+
+            # Write the metadata to the overview CSV file
+            overview_writer.writerow([filename, metadata])
+
+            # Process the parameters tag in the metadata
+            if "parameters" in metadata:
+                parameters = metadata["parameters"].replace("\n", "").replace("\r", "")
+                parameter_dict = {}
+                for param in parameters.split(","):
+                    if ":" in param:
+                        key, value = param.split(":", 1)
+                        parameter_dict[key.strip()] = value.strip()
+                for key, value in parameter_dict.items():
+                    # Update the prompt counts
+                    prompt_counts.setdefault(key, {}).setdefault(value, 0)
+                    prompt_counts[key][value] += 1
+                    # Write the prompt tag counts to the CSV file
+                    prompt_writer.writerow([key, value, prompt_counts[key][value]])
+
+            # Process the negative_prompt tag in the metadata
+            if "negative_prompt" in metadata:
+                negative_prompt = metadata["negative_prompt"].replace("\n", "").replace("\r", "")
+                negative_prompt_dict = {}
+                for param in negative_prompt.split(","):
+                    if ":" in param:
+                        key, value = param.split(":", 1)
+                        negative_prompt_dict[key.strip()] = value.strip()
+                for key, value in negative_prompt_dict.items():
+                    # Update the negative_prompt counts
+                    negative_prompt_counts.setdefault(key, {}).setdefault(value, 0)
+                    negative_prompt_counts[key][value] += 1
+                    # Write the negative_prompt tag counts to the CSV file
+                    negative_prompt_writer.writerow([key, value, negative_prompt_counts[key][value]])
