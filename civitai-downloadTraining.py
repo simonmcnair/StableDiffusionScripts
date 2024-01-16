6f5574630261a3c7d335ec229a58700e61a784fc
101189766+simonmcnair@users.noreply.github.com <101189766+simonmcnair@users.noreply.github.com>
2024-01-16 23:01:47 +0000

    fixes

--- civitai-downloadTraining.py
+++ civitai-downloadTraining.py
@@ -57,18 +57,22 @@ def get_models():
                 print("error.  Site down")
                 exit()
 
+            if response.status_code == 404:
+                print("not found")
+                break
             if response.status_code == 401 and api_key:
                 # Retry the request with the API key
                 headers["Authorization"] = f"Bearer {api_key}"
                 response = requests.get('https://civitai.com/api/v1/models?limit=100&types=LORA', headers=headers, params=params)
 
-                
             if response.status_code == 200:
                 try:
                     data = response.json()
+                    if 'items' in data:
+                        break
                 except Exception as e:
                     write_to_log(logfile_path, "Error " + str(e))
-                break
+                
             else:
                  time.sleep(5)
                  write_to_log(logfile_path, "status code: " + str(response.status_code) + " " + response.reason)
@@ -94,30 +98,53 @@ def get_models():
                             
                             write_to_log(successfile_path, "found training data")
                             
-                            downloadurl = file.get('downloadUrl')
+                            downloadurl = file.get('downloadUrl' )
+                            downloadmodel = file.get('id' )
+                            #downloadurl = f'https://civitai.com/api/download/training-data/:{model_id}'
+                            #downloadurl = f'https://civitai.com/api/download/training-data/{model_id}'
+                            #downloadurl = f'https://civitai.com/api/download/models/:{downloadmodel}?type=Training%20Data'
                             #downloadfilename = name + '_' + model + '_' + file.get('name')
                             downloadfilename = str(model_id) + '_' + str(file.get('id')) + '_' + file.get('name')
-                            download_fullpath = os.path.join(download_to,downloadfilename)        
+                            download_fullpath = os.path.join(download_to,downloadfilename)  
+
+                            #downloadheader = 'attachment; filename="Tags.zip"'      
+                            #headers = {'Content-Type': 'application/json'}
 
                             if not os.path.exists(download_fullpath):
-                                write_to_log(logfile_path, "downloading " + downloadurl + ".  Filename: " + downloadfilename + ". size: " + str(file.get('sizeKB')))
-                                try:
-                                    response = requests.get(downloadurl)
-                                except Exception as e:
-                                    write_to_log(logfile_path, "Error " + str(e))
+                                while True:
+                                    write_to_log(logfile_path, "downloading " + downloadurl + ".  Filename: " + downloadfilename + ". size: " + str(file.get('sizeKB')))
+                                    try:
+                                        #downloadurl2 = downloadurl + '?type=Training%20Data'
+                                        response = requests.get(downloadurl,headers=headers,allow_redirects=True,stream=True)
+                                    except Exception as e:
+                                        write_to_log(logfile_path, "Error " + str(e))
 
-                                if response.status_code == 401 and api_key:
-                                    # Retry the request with the API key
-                                    headers["Authorization"] = f"Bearer {api_key}"
-                                    response = requests.get(downloadurl, headers=headers)
+                                    if response.status_code == 400:
+                                        print("400 file not found")
+                                        break
+                                    if response.status_code == 404:
+                                        print("404 file not found")
+                                        break
 
-                                if response.status_code == 200:
-                                    with open(download_fullpath, 'wb') as file2:
-                                        file2.write(response.content)
-                                    write_to_log(logfile_path, f"File downloaded successfully to {download_fullpath}")
-                                    write_to_log(successfile_path, f"{model},{downloadurl},{downloadfilename}")
-                                else:
-                                    write_to_log(logfile_path, f"Failed to download file. Status code: {response.status_code}")
+                                    if response.status_code == 401 and api_key:
+                                        # Retry the request with the API key
+                                        headers["Authorization"] = f"Bearer {api_key}"
+                                        response = requests.get(downloadurl, headers=headers)
+
+                                    if response.status_code == 200:
+                                        content_disposition = response.headers.get("Content-Disposition", None)
+
+                                        if 'html' in str(response.content):
+                                            print("This is wrong")
+                                            print("This is wrong")
+                                        else:
+                                            with open(download_fullpath, 'wb') as file2:
+                                                file2.write(response.content)
+                                            write_to_log(logfile_path, f"File downloaded successfully to {download_fullpath}")
+                                            write_to_log(successfile_path, f"{model},{downloadurl},{downloadfilename}")
+                                            break
+                                    else:
+                                        write_to_log(logfile_path, f"Failed to download file. Status code: {response.status_code}")
                             else:
                                 write_to_log(logfile_path, "file already exists")
                         else:
