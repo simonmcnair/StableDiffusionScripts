2b9a2738a28581d57d00d9d58002003756fa10dd
101189766+simonmcnair@users.noreply.github.com <101189766+simonmcnair@users.noreply.github.com>
2024-02-02 09:46:03 +0000

    updates

--- json_to_prompt.py
+++ json_to_prompt.py
@@ -9,7 +9,7 @@ def create_prompt(json_data):
     # Your JSON object
 
     #desired_order = ['prompt', 'negativePrompt']
-    desired_order = ['prompt', 'negativePrompt', 'steps', 'sampler', 'cfgScale', 'seed', 'Model', 'Clip skip','Size', 'clipSkip','Model hash','Version']
+    desired_order = ['prompt', 'negativePrompt', 'steps', 'sampler', 'CFG scale', 'cfgScale','seed', 'Model', 'Clip skip','Size', 'clipSkip','Model hash','Version']
     
     mystr = ""
     Model_Hash = None
@@ -20,32 +20,41 @@ def create_prompt(json_data):
     for key in additional_keys:
         if key == 'hashes':
               #, Model hash: 6e7d18a129
-              print(json_data['hashes']['model'])
-              Model_Hash = f"Model hash: {json_data['hashes']['model']}"
+              if 'embed' in json_data:
+                embed_keys = [key.split('embed:', 1)[1] for key in json_data['hashes'] if key.startswith('embed:')]
+                print("test")
 
+              if 'model' in json_data:
+                print(json_data['hashes']['model'])
+                Model_Hash = f"Model hash: {json_data['hashes']['model']}"
+        else:
+             print(f"Additional Key: {key}")
         if key == 'resources':
              for resource in json_data['resources']:
                     if resource['type'] == 'model':
-                        print("hi1")
+                        #print("hi1")
                         #, VAE hash: 63aeecb90f, VAE: vae-ft-mse-840000-ema-pruned.safetensors
                         VAE = f"VAE: {resource['name']}.safetensors, VAE hash: {resource['hash']}"
-                    elif resource['type'] == 'lora':
+                    elif resource['type'] == 'lora' and 'name' in resource and 'weight' in resource:
                         #<lora:33081_282419_annpossible:1>
                         if mystr == None:
-                            print("hi2")
+                            #print("hi2")
                             mystr = f"<lora:{resource['name']}:{resource['weight']}>"
                         else:
-                            print("hi3")
+                            #print("hi3")
                             mystr = f"<lora:{resource['name']}:{resource['weight']}>" + "," + mystr
-                             
+                            
     #formatted_string += ",".join([f"{key}: {json_data[key]}" for key in additional_keys])
+    try:
+        if mystr != None and mystr not in json_data['prompt']:
+            json_data['prompt'] = json_data['prompt'] + ',' + mystr
 
-    if mystr != None and mystr not in json_data['prompt']:
-        json_data['prompt'] = json_data['prompt'] + ',' + mystr
-
-    for key in ['prompt', 'negativePrompt']:
-        if key in json_data:
-            json_data[key] += '\n'
+        for key in ['prompt', 'negativePrompt']:
+            if key in json_data:
+                json_data[key] += '\n'
+    except Exception as e:
+         print(f"error {e}")
+         return False
     # Create a formatted string with "prompt" and "negativePrompt" first
     formatted_string = ",".join([f"{key}: {json_data[key]}" for key in desired_order if key in json_data])
 
@@ -57,6 +66,7 @@ def create_prompt(json_data):
     formatted_string = formatted_string.replace(',clipSkip: ',',Clip skip: ' )
     formatted_string = formatted_string.replace(',ClipSkip: ',',Clip skip: ' )
     formatted_string = formatted_string.replace(',clipskip: ',',Clip skip: ' )
+    formatted_string = formatted_string.replace(',cfgScale: ',',CFG scale: ' )
 
     if Model_Hash != None:
          formatted_string = formatted_string + ',' + Model_Hash
@@ -73,12 +83,12 @@ def walkfoldertree():
 
     headers = {}
     headers['Content-Type'] =  'application/json'
+    force_image_dl = True
 
     for root, dirs, files in os.walk(search_folder):
         for filename in files:
             if '.json' in filename or '.civitai.info' in filename:
                 fullfilepath = os.path.join(root,filename)
-
 
                 #if '.civitai.info' in filename or 'safetensors.json' in filename:
                 if '.civitai.info' in filename :
@@ -91,6 +101,7 @@ def walkfoldertree():
                         json_data = json.load(file)
 
                     base_name, ext = os.path.splitext(originalfilenamebeforeanyperiods)
+                    base_name, ext = os.path.splitext(base_name)
                     if 'id' in json_data and 'modelId' in json_data:
                         id = json_data['id']
                         model_id = json_data['modelId']
@@ -98,33 +109,61 @@ def walkfoldertree():
                         if 'images' in json_data:
                             for index,image in enumerate(json_data['images']):
                                 imagename,imageext = os.path.splitext(image['url'])
-                                metadata = create_prompt(image['meta'])
-                                print(image['url'])
+                                if image['type'] == 'image':
+                                    if 'prompt' in image['meta']:
+                                        metadata = create_prompt(image['meta'])
+                                        if metadata != False:
+                                            print(image['url'])
+                                            png = True
+                                            if png == True:
+                                                 image['url'] = image['url'].replace('.jpeg','.png')
+                                                 image['url'] = image['url'].replace('.jpg','.png')
+                                                 image['url'] = image['url'].replace('/width=450','')
+                                            print(image['url'])
+                                                         
+                                            download_prompt_fullpath = os.path.join(root,f"{model_id}_{id}_{base_name}_{index}.txt")
 
-                                download_image_fullpath = os.path.join(dumpdir,f"{model_id}_{id}_{index}.{imageext}")
-                                download_prompt_fullpath = os.path.join(dumpdir,f"{model_id}_{id}_{index}.txt")
+                                            if not os.path.exists(download_prompt_fullpath):
+                                                with open(download_prompt_fullpath, 'w', newline='\r\n', encoding='utf-8') as file2:
+                                                            file2.write(metadata)
+                                                with open(allprompts, 'a', newline='\r\n', encoding='utf-8') as file2:
+                                                            file2.write(f"{metadata}\r\n")
+                                            else:
+                                                 print("Prompt file already exists")
 
-                                if not os.path.exists(download_prompt_fullpath):
-                                    with open(download_prompt_fullpath, 'w', newline='\r\n', encoding='utf-8') as file2:
-                                                file2.write(metadata)
-                                    with open(allprompts, 'a', newline='\r\n', encoding='utf-8') as file2:
-                                                file2.write(f"{metadata}\r\n")
+                                            response = requests.get(image['url'], headers=headers)
+                                            if response.status_code == 200:
+                                                # The request was successful
+                                                if response.headers['Content-Type'] == 'image/jpeg':
+                                                    print("jpeg")
+                                                    image['url'] = image['url'].replace('.png','.jpeg')
+                                                    #print(response.headers['Content-Type'])
+                                                elif response.headers['Content-Type'] == 'image/png':
+                                                    print("legit PNG")
 
-                                if not os.path.exists(download_image_fullpath):
-                                    response = requests.get(image['url'], headers=headers)
+                                                result = re.search(r'.*\/(.*..*$)', image['url'])
+                                                if result:
+                                                    image_file_name = result.group(1)
+                                                    download_image_fullpath = os.path.join(root,f"{model_id}_{id}_{base_name}_{image_file_name}")
+                                                else:
+                                                    print("could not get filename from URL.")
+                                                    download_image_fullpath = os.path.join(root,f"{model_id}_{id}_{index}{imageext}")
 
-                                    if response.status_code == 200:
-                                        # The request was successful
-                                        file_size = int(response.headers.get("content-length", 0))
-                                        with open(download_image_fullpath, 'wb') as file2:
-                                                    file2.write(response.content)                              
-                                    else:
-                                        print(f"error {response.status_code}" )
+                                                if not os.path.exists(download_image_fullpath):
+                                                    file_size = int(response.headers.get("content-length", 0))
+                                                    with open(download_image_fullpath, 'wb') as file2:
+                                                                file2.write(response.content)
+                                                else:
+                                                    print("image file already exists")                          
+                                            else:print(f"error {response.status_code}" )
+
+                                        else:print(f"erronious metadata")
+                                    else:print("No prompt.  Skipping download of picture")
+                                else:print("not an image")
                     else:
                          print("invalid data")
 
 label_dictionary = []
 search_folder = 'X:/dif/stable-diffusion-webui-docker/data/models/Lora'
-dumpdir = 'c:/users/simon/desktop/test/'
 allprompts = 'c:/users/simon/desktop/test/allprompts.txt'
 walkfoldertree()
--- rename_lora.py
+++ rename_lora.py
@@ -5,6 +5,9 @@ import os
 import re
 import shutil
 import time
+import hashlib
+import io
+from blake3 import blake3
 
 def get_script_name():
     # Use os.path.basename to get the base name (script name) from the full path
@@ -24,6 +27,56 @@ def write_to_log(log_file, message):
     except Exception as e:
         print(f"Error writing to the log file: {e}.  Press a key to continue")
         input()
+
+def calculate_sha256(filename):
+    hash_sha256 = hashlib.sha256()
+    blksize = 1024 * 1024
+
+    with open(filename, "rb") as f:
+        for chunk in iter(lambda: f.read(blksize), b""):
+            hash_sha256.update(chunk)
+
+    return hash_sha256.hexdigest()
+
+def sha256(filename, title, use_addnet_hash=False):
+    print(f"Calculating sha256 for {filename}: ", end='')
+    if use_addnet_hash:
+        with open(filename, "rb") as file:
+            sha256_value = addnet_hash_safetensors(file)
+    else:
+        sha256_value = calculate_sha256(filename)
+    print(f"{sha256_value}")
+
+    return sha256_value
+
+def calculate_hash(file_path):
+    sha1_hash = hashlib.sha1()
+    with open(file_path, "rb") as f:
+        while True:
+            data = f.read(1024)
+            if not data:
+                break
+            sha1_hash.update(data)
+    return sha1_hash.hexdigest()
+
+
+
+def addnet_hash_safetensors(b):
+    """kohya-ss hash for safetensors from https://github.com/kohya-ss/sd-scripts/blob/main/library/train_util.py"""
+    hash_sha256 = hashlib.sha256()
+    blksize = 1024 * 1024
+
+    b.seek(0)
+    header = b.read(8)
+    n = int.from_bytes(header, "little")
+
+    offset = n + 8
+    b.seek(offset)
+    for chunk in iter(lambda: b.read(blksize), b""):
+        hash_sha256.update(chunk)
+
+    return hash_sha256.hexdigest()
+
 
 def sanitise_path_name(folder_name):
     # Define a regular expression pattern to match invalid characters
@@ -158,66 +211,67 @@ def get_lora_prompt(lora_path):
                 combo = str(modelid) + '_' + str(id)
             else:
                 print("Invalid modelid or id for " + lora_path)
-            trainedwords = data_civitai.get("trainedWords")
-            if len(trainedwords) == 0 : 
-                trainedwords = False
-                desc_civitaiini = data_civitai.get("description")
-                if desc_civitaiini != None:
-                    print("no trained words in civitai.info.  Does this help ? " + desc_civitaiini)
-                    if 'trigger' in desc_civitaiini or 'activation' in desc_civitaiini:
-                        print("no trigger found but there is a HIGH probability it is in the description: " + str(desc_civitaiini))
+            
+            if create_activation == True:
+                trainedwords = data_civitai.get("trainedWords")
+                if len(trainedwords) == 0 : 
+                    trainedwords = False
+                    desc_civitaiini = data_civitai.get("description")
+                    if desc_civitaiini != None:
+                        print("no trained words in civitai.info.  Does this help ? " + desc_civitaiini)
+                        if 'trigger' in desc_civitaiini or 'activation' in desc_civitaiini:
+                            print("no trigger found but there is a HIGH probability it is in the description: " + str(desc_civitaiini))
+                        else:
+                            print("no trigger words and no description in .json")
+
                     else:
-                        print("no trigger words and no description in .json")
-
+                        print("no trigger words and no description in civitai.info")
                 else:
-                    print("no trigger words and no description in civitai.info")
-            else:
-                trainedwords = [word.strip() for word in trainedwords[0].split(',')]
-                #trainedwords = [word.strip() for word in trainedwords.split(',')]
-                #trainedwords = [word.strip() for word in trainedwords]
-
-                #trainedwords = ','.join(trainedwords)
+                    trainedwords = [word.strip() for word in trainedwords[0].split(',')]
 
             nfsw = data_civitai.get("model", {}).get("nsfw", None)
-            #nfsw = data_civitai["model"]["nsfw"]
+
+            if get_checksums == True:
+                print("getting checksums")
+                checksums = data_civitai.get("trainedWords")
+
     else:
         print("no .civitai.info file for " + lora_path)
 
-    if os.path.exists(jsonpath):
-        json_file_exists = True
-        with open(jsonpath, 'r', encoding='utf-8') as file:
-            data_json = json.load(file)
-            # Extract the required fields from the JSON data_json
-            preferred_weight = data_json.get("preferred weight", 1)
-            activation_text = data_json.get("activation text")
-            if activation_text == '' or activation_text == None :
-                activation_text = False
+    if create_activation == True:
+        if os.path.exists(jsonpath):
+            json_file_exists = True
+            with open(jsonpath, 'r', encoding='utf-8') as file:
+                data_json = json.load(file)
+                # Extract the required fields from the JSON data_json
+                preferred_weight = data_json.get("preferred weight", 1)
+                activation_text = data_json.get("activation text")
+                if activation_text == '' or activation_text == None :
+                    activation_text = False
 
-                desc_json = data_json.get("description")
-                if desc_json != None:
-                    desc_json = desc_json.lower()
-                    if 'trigger' in desc_json or 'activation' in desc_json:
-                        print("no trigger found but there is a HIGH probability it is in the description: " + str(desc_json))
-                    else:
-                        print("no trigger words and no description in .json")
+                    desc_json = data_json.get("description")
+                    if desc_json != None:
+                        desc_json = desc_json.lower()
+                        if 'trigger' in desc_json or 'activation' in desc_json:
+                            print("no trigger found but there is a HIGH probability it is in the description: " + str(desc_json))
+                        else:
+                            print("no trigger words and no description in .json")
 
-            else:
-                activation_text = [word.strip() for word in activation_text.split(',')]
+                else:
+                    activation_text = [word.strip() for word in activation_text.split(',')]
 
-            try:
-                if float(preferred_weight) == 0:
+                try:
+                    if float(preferred_weight) == 0:
+                        preferred_weight = 1
+                except:
                     preferred_weight = 1
-            except:
-                preferred_weight = 1
-    else:
-       print("no json file for " + lora_path)
+        else:
+            print("no json file for " + lora_path)
 
-    if civit_file_exists == False and json_file_exists == False:
-        print("This shouldn't happen")
-    if trainedwords == False and activation_text == False:
-        print(lora_path + " no activation words")
-        print(lora_path + " no activation words")
-
+    if create_activation == True:
+        if trainedwords == False and activation_text == False:
+            print(lora_path + " no activation words")
+            print(lora_path + " no activation words")
         
     #lora_name = Path(lora_path).stem
 
@@ -227,13 +281,14 @@ def main():
     global root_directory
     root_directory = os.path.normpath(root_directory)
 
+
     for root, dirs, files in os.walk(root_directory):
         for filename in files:
             if filename.endswith('.safetensors'):
                 fullpath = os.path.normpath(os.path.join(root, filename))
                 ret,actwrd1,actwrd2,nsfw,weight = get_lora_prompt(fullpath)
                 
-                if ret != False:
+                if ret != False and rename_Loras == True:
                     rename_file_grouping(fullpath,ret)
                     #time.sleep(4)
                 else:
@@ -250,12 +305,19 @@ def main():
                     print("no activation text")
                     unique_elements = ['NOTRIGGER']
                 #write_to_log(log_file,f"{fullpath},{unique_elements}")
-                write_to_log(log_file,f"{fullpath},{','.join(unique_elements)}")
+                    
+                    wrapped_string = f'"{",".join(unique_elements)}"'
+
+                if create_activation == True:
+                    write_to_log(Trigger_file,f"{fullpath},{wrapped_string},{nsfw},{weight}")
 
 
 
 root_directory = '/file/to/sort/'
 useapikey = False
+rename_Loras = False
+create_activation = True
+get_checksums = True
 
 if useapikey == True:
     #unused here
@@ -274,4 +336,5 @@ else:
 
 debug = True
 log_file = os.path.join(get_script_path(),get_script_name() + '.log')
+Trigger_file = os.path.join(get_script_path(),'trigger.txt')
 main()
