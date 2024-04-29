4693c73de3c381f7453e2d99a25df7e79bd18cda
101189766+simonmcnair@users.noreply.github.com <101189766+simonmcnair@users.noreply.github.com>
2024-04-29 17:23:27 +0100

    test

--- imageclassification2.py
+++ imageclassification2.py
@@ -1623,15 +1623,15 @@ else:
                                     #     logger.info(f"model {model_name} already loaded")
 
                                 #    image = Image.open(fullpath).convert('RGB')
-
-                                    processor = BlipProcessor.from_pretrained(desc)
-                                    model = BlipForConditionalGeneration.from_pretrained(desc)
-                                    image = Image.open(fullpath).convert('RGB')
-                                    inputs = processor(image, return_tensors="pt")
-                                    out = model.generate(**inputs)
-                                    logger.info(f"{fullpath}. {each} {processor.decode(out[0], skip_special_tokens=True)}")
-                                    logger.info("press a key to continue")
-                                    input()
+                                    for each,desc in modelarray.items():
+                                        processor = BlipProcessor.from_pretrained(desc)
+                                        model = BlipForConditionalGeneration.from_pretrained(desc)
+                                        image = Image.open(fullpath).convert('RGB')
+                                        inputs = processor(image, return_tensors="pt")
+                                        out = model.generate(**inputs)
+                                        logger.info(f"{fullpath}. {each} {processor.decode(out[0], skip_special_tokens=True)}")
+                                        logger.info("press a key to continue")
+                                        input()
 
                                     # caption_model_name = 'blip-large' #@param ["blip-base", "blip-large", "git-large-coco"]
                                     # clip_model_name = 'ViT-L-14/openai' #@param ["ViT-L-14/openai", "ViT-H-14/laion2b_s32b_b79k"]
