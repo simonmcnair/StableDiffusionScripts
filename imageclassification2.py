bc3e574a355dfc06dfcef07627b32f8f6fa629e1
101189766+simonmcnair@users.noreply.github.com <101189766+simonmcnair@users.noreply.github.com>
2024-04-29 15:08:09 +0100

    silly mistakes

--- imageclassification2.py
+++ imageclassification2.py
@@ -1625,13 +1625,14 @@ else:
                                     caption_model_name = 'blip-large' #@param ["blip-base", "blip-large", "git-large-coco"]
                                     clip_model_name = 'ViT-L-14/openai' #@param ["ViT-L-14/openai", "ViT-H-14/laion2b_s32b_b79k"]
 
-                                    ci.config.chunk_size = 2048 if ci.config.clip_model_name == "ViT-L-14/openai" else 1024
-                                    ci.config.flavor_intermediate_count = 2048 if ci.config.clip_model_name == "ViT-L-14/openai" else 1024
-
                                     config = Config()
+
                                     config.clip_model_name = clip_model_name
                                     config.caption_model_name = caption_model_name
                                     ci = Interrogator(config)
+
+                                    ci.config.chunk_size = 2048 if ci.config.clip_model_name == "ViT-L-14/openai" else 1024
+                                    ci.config.flavor_intermediate_count = 2048 if ci.config.clip_model_name == "ViT-L-14/openai" else 1024
 
                                     #ci = Interrogator(Config(clip_model_name="ViT-L-14/openai"))
 
